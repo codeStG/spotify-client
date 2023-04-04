7a5f583510ca3903014a61a855e45a49a1b3443a
Torres <getorres@teksystems.com>
2023-04-04 10:19:05 -0500

    Initializing new Spotify Client project

---
+++ src/main/java/com/stgcodes/client/spotify/SpotifyApiClientApplication.java
@@ -0,0 +1,13 @@
+package com.stgcodes.client.spotify;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpotifyApiClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpotifyApiClientApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/stgcodes/client/spotify/SpotifyApiClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stgcodes.client.spotify;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpotifyApiClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
