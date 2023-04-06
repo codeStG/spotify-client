37f3d7c6735c9b1603251e01613462bf85f41e78
Torres <getorres@teksystems.com>
2023-04-06 11:51:44 -0500

    Removed logging used for debugging

--- src/main/java/com/stgcodes/client/spotify/service/ArtistService.java
+++ src/main/java/com/stgcodes/client/spotify/service/ArtistService.java
@@ -1,7 +1,5 @@
 package com.stgcodes.client.spotify.service;
 
-import com.stgcodes.client.spotify.model.Artist;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Mono;
@@ -9,7 +7,6 @@ import reactor.core.publisher.Mono;
 import static org.springframework.security.oauth2.client.web.reactive.function.client.ServerOAuth2AuthorizedClientExchangeFilterFunction.clientRegistrationId;
 
 @Service
-@Slf4j
 public class ArtistService {
     private final WebClient webClient;
 
@@ -18,7 +15,7 @@ public class ArtistService {
     }
 
     public Mono<String> findById(String id) {
-        log.info(String.valueOf(clientRegistrationId("spotify")));
+
         return webClient.get()
                 .uri("/artists/{id}", id)
                 .attributes(clientRegistrationId("spotify"))
