b5a6b3d5f74673c76cf68fb895b6d663c85932af
Torres <getorres@teksystems.com>
2023-04-06 11:38:17 -0500

    updating oauth2client controller

--- src/main/java/com/stgcodes/client/spotify/controller/OAuth2ClientController.java
+++ src/main/java/com/stgcodes/client/spotify/controller/OAuth2ClientController.java
@@ -1,6 +1,8 @@
 package com.stgcodes.client.spotify.controller;
 
-import org.springframework.security.oauth2.client.registration.ReactiveClientRegistrationRepository;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClient;
+import org.springframework.security.oauth2.client.ReactiveOAuth2AuthorizedClientService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Mono;
@@ -8,14 +10,16 @@ import reactor.core.publisher.Mono;
 @RestController
 public class OAuth2ClientController {
 
-    private final ReactiveClientRegistrationRepository clientRegistrationRepository;
+    private final ReactiveOAuth2AuthorizedClientService authorizedClientService;
 
-    public OAuth2ClientController(ReactiveClientRegistrationRepository clientRegistrationRepository) {
-        this.clientRegistrationRepository = clientRegistrationRepository;
+    public OAuth2ClientController(ReactiveOAuth2AuthorizedClientService authorizedClientService) {
+        this.authorizedClientService = authorizedClientService;
     }
 
     @GetMapping("/")
-    public Mono<String> index() {
-        return this.clientRegistrationRepository.findByRegistrationId("spotify").thenReturn("index");
+    public Mono<String> index(Authentication authentication) {
+        return this.authorizedClientService.loadAuthorizedClient("spotify", authentication.getName())
+                .map(OAuth2AuthorizedClient::getAccessToken)
+                .thenReturn("index");
     }
 }
