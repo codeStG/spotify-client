4e6373bf52632fa814f70391b79aa86c4ef0095b
Torres <getorres@teksystems.com>
2023-04-12 10:09:37 -0500

    Created generic BaseService

--- src/main/java/com/stgcodes/client/spotify/service/ArtistService.java
+++ src/main/java/com/stgcodes/client/spotify/service/ArtistService.java
@@ -1,26 +1,13 @@
 package com.stgcodes.client.spotify.service;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.stgcodes.client.spotify.model.Artist;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
-import reactor.core.publisher.Mono;
-
-import static org.springframework.security.oauth2.client.web.reactive.function.client.ServerOAuth2AuthorizedClientExchangeFilterFunction.clientRegistrationId;
 
 @Service
-public class ArtistService {
-    private final WebClient webClient;
-
-    public ArtistService(WebClient webClient) {
-        this.webClient = webClient;
-    }
-
-    public Mono<Artist> findById(String id) {
-
-        return webClient.get()
-                .uri("/artists/{id}", id)
-                .attributes(clientRegistrationId("spotify"))
-                .retrieve()
-                .bodyToMono(Artist.class);
+public class ArtistService extends BaseService<Artist> {
+    public ArtistService(WebClient webClient, ObjectMapper mapper) {
+        super(webClient, mapper, Artist.class);
     }
 }
