6db4c32daad4ca34dc7588841c57bcf8447918d8
Torres <getorres@teksystems.com>
2023-04-13 14:38:22 -0500

    Added follower count to Artist model

--- src/main/java/com/stgcodes/client/spotify/model/Artist.java
+++ src/main/java/com/stgcodes/client/spotify/model/Artist.java
@@ -1,13 +1,21 @@
 package com.stgcodes.client.spotify.model;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 
 import java.util.List;
+import java.util.Map;
 
 @Data
 public class Artist {
     private String id;
     private String name;
     private int popularity;
+    private int totalFollowers;
     private List<String> genres;
+
+    @JsonProperty("followers")
+    private void unpackFollowerCount(Map<String, Integer> followers) {
+        totalFollowers = followers.get("total");
+    }
 }
