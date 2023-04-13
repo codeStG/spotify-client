c2a99c958646d626defae98aeb4574368c249230
Torres <getorres@teksystems.com>
2023-04-13 15:03:51 -0500

    Creating rough Album model

--- src/main/java/com/stgcodes/client/spotify/model/Album.java
+++ src/main/java/com/stgcodes/client/spotify/model/Album.java
@@ -1,4 +1,20 @@
 package com.stgcodes.client.spotify.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Data;
+
+import java.util.List;
+
+@Data
 public class Album {
+    private String id;
+
+    private String name;
+
+    @JsonProperty("total_tracks")
+    private int totalTracks;
+
+    @JsonIgnoreProperties(value = { "popularity", "totalFollowers", "genres" })
+    private List<Artist> artists;
 }
