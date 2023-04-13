11d7a8b47bb9ad19d15e23df24a25cb4f60aaedc
Torres <getorres@teksystems.com>
2023-04-13 14:14:20 -0500

    Created findAll method for Artist endpoint

--- src/main/java/com/stgcodes/client/spotify/service/ArtistService.java
+++ src/main/java/com/stgcodes/client/spotify/service/ArtistService.java
@@ -1,13 +1,38 @@
 package com.stgcodes.client.spotify.service;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.stgcodes.client.spotify.model.Artist;
+import com.stgcodes.client.spotify.model.ArtistsWrapper;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
+
+import java.util.List;
+
+import static org.springframework.security.oauth2.client.web.reactive.function.client.ServerOAuth2AuthorizedClientExchangeFilterFunction.clientRegistrationId;
 
 @Service
-public class ArtistService extends BaseService<Artist> {
-    public ArtistService(WebClient webClient, ObjectMapper mapper) {
-        super(webClient, mapper, Artist.class);
+public class ArtistService {
+    private final WebClient webClient;
+
+    public ArtistService(WebClient webClient) {
+        this.webClient = webClient;
+    }
+
+    public Mono<Artist> findById(String id) {
+
+        return webClient.get()
+                .uri("/artists/{id}", id)
+                .attributes(clientRegistrationId("spotify"))
+                .retrieve()
+                .bodyToMono(Artist.class);
+    }
+
+    public Mono<List<Artist>> findAll(String ids) {
+        return webClient.get()
+                .uri("/artists?ids={ids}", ids)
+                .attributes(clientRegistrationId("spotify"))
+                .retrieve()
+                .bodyToMono(ArtistsWrapper.class)
+                .map(ArtistsWrapper::getArtists);
     }
 }
