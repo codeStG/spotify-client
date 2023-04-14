3cba86c8c543fd0ea1c6c6c847563859d4c41713
Torres <getorres@teksystems.com>
2023-04-14 12:07:31 -0500

    Adding releaseDate field to Album model

--- src/main/java/com/stgcodes/client/spotify/model/Album.java
+++ src/main/java/com/stgcodes/client/spotify/model/Album.java
@@ -1,10 +1,12 @@
 package com.stgcodes.client.spotify.model;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.databind.PropertyNamingStrategies;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
 import lombok.Data;
 
+import java.time.LocalDate;
 import java.util.List;
 
 @Data
@@ -15,6 +17,9 @@ public class Album {
     private String name;
 
     private int totalTracks;
+
+    @JsonFormat(pattern = "uuuu-MM-dd")
+    private LocalDate releaseDate;
 
     @JsonIgnoreProperties(value = { "popularity", "total_followers", "genres" })
     private List<Artist> artists;
