e47262afc028211427cf8d4dfe96d2486a218c8c
Torres <getorres@teksystems.com>
2023-04-14 12:22:50 -0500

    Optimizing imports

--- src/main/java/com/stgcodes/client/spotify/controller/AlbumController.java
+++ src/main/java/com/stgcodes/client/spotify/controller/AlbumController.java
@@ -1,9 +1,7 @@
 package com.stgcodes.client.spotify.controller;
 
 import com.stgcodes.client.spotify.model.Album;
-import com.stgcodes.client.spotify.model.Artist;
 import com.stgcodes.client.spotify.service.AlbumService;
-import com.stgcodes.client.spotify.service.ArtistService;
 import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Mono;
 
@@ -23,5 +21,11 @@ public class AlbumController {
     @ResponseBody Mono<Album>
     findById(@RequestParam(value = "id", defaultValue = "") String id) {
         return service.findById(id);
+    }
+
+    @GetMapping(params = "ids")
+    @ResponseBody Mono<List<Album>>
+    findAll(@RequestParam(value = "ids", defaultValue = "") String ids) {
+        return service.findAll(ids);
     }
 }
