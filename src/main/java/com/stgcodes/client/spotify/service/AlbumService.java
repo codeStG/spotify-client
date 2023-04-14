ce4796a367da30c0691bacea7f54ec252aba276e
Torres <getorres@teksystems.com>
2023-04-14 12:18:47 -0500

    Created findAll method for albums

--- src/main/java/com/stgcodes/client/spotify/service/AlbumService.java
+++ src/main/java/com/stgcodes/client/spotify/service/AlbumService.java
@@ -1,10 +1,12 @@
 package com.stgcodes.client.spotify.service;
 
 import com.stgcodes.client.spotify.model.Album;
-import com.stgcodes.client.spotify.model.Artist;
+import com.stgcodes.client.spotify.model.AlbumsWrapper;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Mono;
+
+import java.util.List;
 
 import static org.springframework.security.oauth2.client.web.reactive.function.client.ServerOAuth2AuthorizedClientExchangeFilterFunction.clientRegistrationId;
 
@@ -22,5 +24,14 @@ public class AlbumService {
                 .attributes(clientRegistrationId("spotify"))
                 .retrieve()
                 .bodyToMono(Album.class);
+    }
+
+    public Mono<List<Album>> findAll(String ids) {
+        return webClient.get()
+                .uri("/albums?ids={ids}", ids)
+                .attributes(clientRegistrationId("spotify"))
+                .retrieve()
+                .bodyToMono(AlbumsWrapper.class)
+                .map(AlbumsWrapper::getAlbums);
     }
 }
