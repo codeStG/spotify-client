0ac9b7c54ffc5638bfee0c187ba273f08db681d2
Torres <getorres@teksystems.com>
2023-04-17 12:52:44 -0500

    Created service method to find by multiple ids

--- src/main/java/com/stgcodes/client/spotify/service/TrackService.java
+++ src/main/java/com/stgcodes/client/spotify/service/TrackService.java
@@ -1,9 +1,14 @@
 package com.stgcodes.client.spotify.service;
 
+import com.stgcodes.client.spotify.model.Album;
+import com.stgcodes.client.spotify.model.AlbumsWrapper;
 import com.stgcodes.client.spotify.model.Track;
+import com.stgcodes.client.spotify.model.TracksWrapper;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Mono;
+
+import java.util.List;
 
 import static org.springframework.security.oauth2.client.web.reactive.function.client.ServerOAuth2AuthorizedClientExchangeFilterFunction.clientRegistrationId;
 
@@ -22,5 +27,14 @@ public class TrackService {
                 .attributes(clientRegistrationId("spotify"))
                 .retrieve()
                 .bodyToMono(Track.class);
+    }
+
+    public Mono<List<Track>> findAll(String ids) {
+        return webClient.get()
+                .uri("/tracks?ids={ids}", ids)
+                .attributes(clientRegistrationId("spotify"))
+                .retrieve()
+                .bodyToMono(TracksWrapper.class)
+                .map(TracksWrapper::getTracks);
     }
 }
