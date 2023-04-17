5eee200f9a17d430d11f31a4ff67cc54224707c1
Torres <getorres@teksystems.com>
2023-04-17 12:53:17 -0500

    Created controller find by multiple ids method

--- src/main/java/com/stgcodes/client/spotify/controller/TrackController.java
+++ src/main/java/com/stgcodes/client/spotify/controller/TrackController.java
@@ -1,9 +1,12 @@
 package com.stgcodes.client.spotify.controller;
 
+import com.stgcodes.client.spotify.model.Album;
 import com.stgcodes.client.spotify.model.Track;
 import com.stgcodes.client.spotify.service.TrackService;
 import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Mono;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/track")
@@ -20,5 +23,11 @@ public class TrackController {
     Mono<Track>
     findById(@RequestParam(value = "id", defaultValue = "") String id) {
         return service.findById(id);
+    }
+
+    @GetMapping(params = "ids")
+    @ResponseBody Mono<List<Track>>
+    findAll(@RequestParam(value = "ids", defaultValue = "") String ids) {
+        return service.findAll(ids);
     }
 }
