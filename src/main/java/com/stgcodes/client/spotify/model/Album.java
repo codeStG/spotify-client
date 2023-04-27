3731ab5f72c6557e15a3d46355b437ba1528a201
George <stgcodes@gmail.com>
2023-04-27 15:44:24 -0500

    Added Lombok Builder and removed date deserializer

--- src/main/java/com/stgcodes/client/spotify/model/Album.java
+++ src/main/java/com/stgcodes/client/spotify/model/Album.java
@@ -4,16 +4,16 @@ import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.databind.PropertyNamingStrategies;
-import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
-import com.stgcodes.client.spotify.deserializer.CustomDateDeserializer;
 import com.stgcodes.client.spotify.model.wrapper.TracksWrapper;
+import lombok.Builder;
 import lombok.Data;
 
 import java.time.LocalDate;
 import java.util.List;
 
 @Data
+@Builder
 @JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
 public class Album {
     private String id;
@@ -25,7 +25,6 @@ public class Album {
     private int popularity;
 
     @JsonFormat(pattern = "uuuu-MM-dd")
-    @JsonDeserialize(using = CustomDateDeserializer.class)
     private LocalDate releaseDate;
 
     @JsonIgnoreProperties(value = { "popularity", "total_followers", "genres" })
