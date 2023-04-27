1a7cfa686452c7c0a001ea20d321f33e182a7657
George <stgcodes@gmail.com>
2023-04-27 15:58:14 -0500

    Creating Artist entity to mirror artist response from external API

--- src/main/java/com/stgcodes/client/spotify/entity/ArtistEntity.java
+++ src/main/java/com/stgcodes/client/spotify/entity/ArtistEntity.java
@@ -1,16 +1,40 @@
 package com.stgcodes.client.spotify.entity;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
-
-import java.util.List;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.Field;
 
 @Data
-@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Document(collection = "artists")
 public class ArtistEntity {
+
+    @Id
     private String id;
+
+    private String[] genres;
+
+    @Field("external_urls")
+    @JsonProperty("external_urls")
+    private Object externalUrls;
+
+    private Object followers;
+
+    private String href;
+
+    private Object[] images;
+
     private String name;
+
     private int popularity;
-    private int totalFollowers;
-    private List<String> genres;
+
+    private String type;
+
+    private String uri;
 }
