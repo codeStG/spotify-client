258ed00fd946f34b46025ad5e08a8dd77f661cc2
George <stgcodes@gmail.com>
2023-04-27 15:43:46 -0500

    Added every field to entity - added custom deserializer to date field

--- src/main/java/com/stgcodes/client/spotify/entity/AlbumEntity.java
+++ src/main/java/com/stgcodes/client/spotify/entity/AlbumEntity.java
@@ -1,18 +1,18 @@
 package com.stgcodes.client.spotify.entity;
 
-import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.stgcodes.client.spotify.deserializer.CustomDateDeserializer;
 import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.Field;
 
 import java.time.LocalDate;
-import java.util.List;
 
 @Data
-@Builder
 @NoArgsConstructor
 @AllArgsConstructor
 @Document(collection = "albums")
@@ -23,14 +23,52 @@ public class AlbumEntity {
 
     private String name;
 
+    @Field("album_type")
+    @JsonProperty("album_type")
+    private String albumType;
+
+    @Field("total_tracks")
+    @JsonProperty("total_tracks")
     private int totalTracks;
+
+    @Field("available_markets")
+    @JsonProperty("available_markets")
+    private String[] availableMarkets;
+
+    @Field("external_urls")
+    @JsonProperty("external_urls")
+    private Object externalUrls;
+
+    private String href;
+
+    private Object[] images;
+
+    @Field("release_date")
+    @JsonProperty("release_date")
+    @JsonDeserialize(using = CustomDateDeserializer.class)
+    private LocalDate releaseDate;
+
+    @Field("release_date_precision")
+    @JsonProperty("release_date_precision")
+    private String releaseDatePrecision;
+
+    private String type;
+
+    private String uri;
+
+    private Object[] copyrights;
+
+    @Field("external_ids")
+    @JsonProperty("external_ids")
+    private Object externalsIds;
+
+    private String[] genres;
+
+    private String label;
 
     private int popularity;
 
-    @JsonFormat(pattern = "uuuu-MM-dd")
-    private LocalDate releaseDate;
+    private Object[] artists;
 
-    private List<ArtistEntity> artists;
-
-    private List<TrackEntity> tracks;
+    private Object tracks;
 }
