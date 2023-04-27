8da827f2e088ca3b475310ff992d348617dbacb1
George <stgcodes@gmail.com>
2023-04-27 18:18:44 -0500

    Adding Lombok builder to Track model

--- src/main/java/com/stgcodes/client/spotify/model/Track.java
+++ src/main/java/com/stgcodes/client/spotify/model/Track.java
@@ -3,11 +3,13 @@ package com.stgcodes.client.spotify.model;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.databind.PropertyNamingStrategies;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
+import lombok.Builder;
 import lombok.Data;
 
 import java.util.List;
 
 @Data
+@Builder
 @JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class Track {
