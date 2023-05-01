383560fc6292da7d5f576c5eeb5e25f5542f6f98
George <stgcodes@gmail.com>
2023-05-01 14:48:25 -0500

    Removing unused imports

--- src/main/java/com/stgcodes/client/spotify/service/GenericService.java
+++ src/main/java/com/stgcodes/client/spotify/service/GenericService.java
@@ -1,19 +1,7 @@
 package com.stgcodes.client.spotify.service;
 
-import com.stgcodes.client.spotify.entity.AlbumEntity;
-import com.stgcodes.client.spotify.entity.ArtistEntity;
-import com.stgcodes.client.spotify.entity.TrackEntity;
-import com.stgcodes.client.spotify.model.Album;
-import com.stgcodes.client.spotify.model.Artist;
-import com.stgcodes.client.spotify.model.Track;
-import com.stgcodes.client.spotify.model.simple.SimpleAlbum;
-import com.stgcodes.client.spotify.model.simple.SimpleArtist;
-import com.stgcodes.client.spotify.model.simple.SimpleTrack;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Mono;
-
-import java.util.ArrayList;
-import java.util.List;
 
 import static org.springframework.security.oauth2.client.web.reactive.function.client.ServerOAuth2AuthorizedClientExchangeFilterFunction.clientRegistrationId;
 
