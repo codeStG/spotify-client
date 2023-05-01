4dfb3e9621c2039e39c6b6f36ceeb633ac690b34
George <stgcodes@gmail.com>
2023-05-01 12:52:28 -0500

    Added Lombok Data & Builder

--- src/main/java/com/stgcodes/client/spotify/model/simple/SimpleArtist.java
+++ src/main/java/com/stgcodes/client/spotify/model/simple/SimpleArtist.java
@@ -1,5 +1,10 @@
 package com.stgcodes.client.spotify.model.simple;
 
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
 public class SimpleArtist {
     private String id;
     private String name;
