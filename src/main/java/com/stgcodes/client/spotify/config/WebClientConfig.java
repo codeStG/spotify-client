f1cdf1a723afe91c540f5b5e6958377876df24e5
Torres <getorres@teksystems.com>
2023-05-04 10:17:11 -0500

    Changed name of base uri property

--- src/main/java/com/stgcodes/client/spotify/config/WebClientConfig.java
+++ src/main/java/com/stgcodes/client/spotify/config/WebClientConfig.java
@@ -13,7 +13,7 @@ import org.springframework.web.reactive.function.client.WebClient;
 @Configuration
 public class WebClientConfig {
 
-    @Value("${spotify.base-url}")
+    @Value("${spotify.uri.base}")
     private String baseUrl;
 
     @Bean
