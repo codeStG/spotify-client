3caea4bc43900e784b780d227ae2f9ffcbeeb3bd
George <stgcodes@gmail.com>
2023-05-15 16:52:21 -0500

    Adding Lombok Builder to models

--- src/main/java/com/stgcodes/client/spotify/dto/AlbumDto.java
+++ src/main/java/com/stgcodes/client/spotify/dto/AlbumDto.java
@@ -2,12 +2,18 @@ package com.stgcodes.client.spotify.dto;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDate;
 import java.util.List;
 
 @Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class AlbumDto {
     private String id;
 
